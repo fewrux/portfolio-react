ea14396edcb2d6edda49120be61593332600ecc9
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-07-03 21:48:17 -0300

    update resume and tech

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -5,12 +5,12 @@ export function Technologies() {
       <div className="logo-carousel mx-auto p-3">
         <ul className="container d-flex p-0 justify-content-center flex-wrap gap-5 mt-5 px-3">
           <li className="hover text-center text-primary">
-            <i className="devicon-ruby-plain fs-50"></i>
-            <p className="m-0">Ruby</p>
+            <i className="devicon-rust-plain fs-50"></i>
+            <p className="m-0">Rust</p>
           </li>
           <li className="hover text-center text-primary">
-            <i className="devicon-rails-plain fs-50"></i>
-            <p className="m-0">Rails</p>
+            <i className="devicon-solidity-plain fs-50"></i>
+            <p className="m-0">Solidity</p>
           </li>
           <li className="hover text-center text-primary">
             <i className="devicon-javascript-plain fs-50"></i>
@@ -21,36 +21,48 @@ export function Technologies() {
             <p className="m-0">TypeScript</p>
           </li>
           <li className="hover text-center text-primary">
+            <i className="devicon-nodejs-plain fs-50"></i>
+            <p className="m-0">Node.js</p>
+          </li>
+          <li className="hover text-center text-primary">
+            <i className="devicon-nestjs-plain fs-50"></i>
+            <p className="m-0">NestJS</p>
+          </li>
+          <li className="hover text-center text-primary">
             <i className="devicon-react-original fs-50"></i>
             <p className="m-0">React</p>
           </li>
           <li className="hover text-center text-primary">
             <i className="devicon-nextjs-original fs-50"></i>
-            <p className="m-0">NextJS</p>
+            <p className="m-0">Next.js</p>
           </li>
           <li className="hover text-center text-primary">
-            <i className="devicon-nodejs-plain fs-50"></i>
-            <p className="m-0">NodeJS</p>
+            <i className="devicon-ruby-plain fs-50"></i>
+            <p className="m-0">Ruby</p>
           </li>
           <li className="hover text-center text-primary">
-            <i className="devicon-html5-plain fs-50"></i>
-            <p className="m-0">HTML5</p>
+            <i className="devicon-rails-plain fs-50"></i>
+            <p className="m-0">Rails</p>
           </li>
           <li className="hover text-center text-primary">
-            <i className="devicon-css3-plain fs-50"></i>
-            <p className="m-0">CSS3</p>
+            <i className="devicon-docker-plain fs-50"></i>
+            <p className="m-0">Docker</p>
           </li>
           <li className="hover text-center text-primary">
-            <i className="devicon-bootstrap-plain fs-50"></i>
-            <p className="m-0">Bootstrap</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-sass-original fs-50"></i>
-            <p className="m-0">Sass</p>
+            <i className="devicon-linux-plain fs-50"></i>
+            <p className="m-0">Linux</p>
           </li>
           <li className="hover text-center text-primary">
             <i className="devicon-postgresql-plain fs-50"></i>
             <p className="m-0">Postgre</p>
+          </li>
+          <li className="hover text-center text-primary">
+            <i className="devicon-mysql-plain fs-50"></i>
+            <p className="m-0">MySQL</p>
+          </li>
+          <li className="hover text-center text-primary">
+            <i className="devicon-microsoftsqlserver-plain fs-50"></i>
+            <p className="m-0">SQLServer</p>
           </li>
           <li className="hover text-center text-primary">
             <i className="devicon-mongodb-plain fs-50"></i>
@@ -63,30 +75,6 @@ export function Technologies() {
           <li className="hover text-center text-primary">
             <i className="devicon-graphql-plain fs-50"></i>
             <p className="m-0">GraphQL</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-webpack-plain fs-50"></i>
-            <p className="m-0">Webpack</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-git-plain fs-50"></i>
-            <p className="m-0">Git</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-github-original fs-50"></i>
-            <p className="m-0">GitHub</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-yarn-plain fs-50"></i>
-            <p className="m-0">Yarn</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-heroku-original fs-50"></i>
-            <p className="m-0">Heroku</p>
-          </li>
-          <li className="hover text-center text-primary">
-            <i className="devicon-figma-plain fs-50"></i>
-            <p className="m-0">Figma</p>
           </li>
         </ul>
       </div>
